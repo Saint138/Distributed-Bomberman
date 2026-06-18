2a583cfa0d7d8fca580b7fe530999f9013b252ce
Giovanni Rinchiuso <giovanni.rinchiuso@studio.unibo.it>
2026-06-18 16:40:17 +0100

    Deleted unuseful file

--- start.sh
+++
@@ -1,9 +0,0 @@
-taskkill //F //IM py.exe 2>/dev/null
-taskkill //F //IM python.exe 2>/dev/null
-sleep 1
-start py src/server/mainServer.py
-sleep 2
-start py src/server/fault_tolerance/proxy_server.py
-sleep 1
-start py src/client/mainClient.py
-start py src/client/mainClient.py
